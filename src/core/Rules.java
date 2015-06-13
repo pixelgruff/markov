9e89692dea5a5c0e10cd58cdbd6c9fa93d2e32cc
Stepchild <lee.matthan@gmail.com>
2015-06-13 16:45:41 -0400

    Initial commit for the Rules interface.  Still a work in progress, but applied successfully in the tictactoe package.

---
+++ src/core/Rules.java
@@ -0,0 +1,61 @@
+package core;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author Ginger Rules holds the transition model and other game-specific logic
+ * for identifying and taking appropriate actions.
+ * @param <S>
+ * @param <A>
+ */
+public interface Rules<S extends State, A extends Action> {
+
+    /**
+     * Calculate a new state (S') from a state-action pair (S, A) according to
+     * the transition model inherent to these game rules
+     * @param state Origin State
+     * @param action Action taken in origin state
+     * @return Resultant State
+     */
+    public S transition(final S state, final A action);
+
+    /**
+     * Calculate all the valid actions for a particular player at a particular
+     * game 
+     * @param state Origin State
+     * @param player Player whose actions need to be enumerated
+     * @return All valid actions for this player from this state
+     */
+    public Collection<A> getAvailableActions(final Player player, final S state);
+    
+    /**
+     * Check if a state is terminal, and the simulation should stop
+     * @param state State to check
+     * @return Whether or not the state is terminal
+     */
+    public boolean isTerminal(final S state);
+    
+    /**
+     * Extract a reference to the current player from the state
+     * @param state
+     * @return 
+     */
+    public Player getCurrentPlayer(final S state);
+    
+    /**
+     * Filter a state's information to tailor it to a particular Player's view
+     * @param state
+     * @param p
+     * @return 
+     */
+    public S filterState(final S state, final Player p);
+    
+    /**
+     * Score a player at a particular game state
+     * @param state
+     * @param p
+     * @return 
+     */
+    public double score(final S state, final Player p);
+}
