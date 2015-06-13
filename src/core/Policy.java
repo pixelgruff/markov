2b528949ad4b42a84ec6695116e33d7ec0dd12b7
Stepchild <lee.matthan@gmail.com>
2015-06-13 16:44:54 -0400

    Syntax refactor for personal clarity.  Should we use alphabetical order for signatures that contain generic types, or just do WHATEVER WE WANT

--- src/core/Policy.java
+++ src/core/Policy.java
@@ -5,12 +5,12 @@ import java.util.Collection;
  *
  * @author Ginger Policies map states to actions; users will extend this class
  *         to implement their AI.
- * @param <A>
- *            Action type for the game
  * @param <S>
  *            State type for the action
+ * @param <A>
+ *            Action type for the game
  */
-public interface Policy<A extends Action, S extends State<A>>
+public interface Policy<S extends State, A extends Action>
 {
     public A chooseAction(S state, Collection<A> actions);
 }
