57b8f08ba0d9ee813ec4f6d29b05ee787a93a644
Stepchild <lee.matthan@gmail.com>
2015-06-13 16:46:46 -0400

    The Rules refactor moved all the game logic out of State, leaving it a barren wasteland.  The goal is for State and Actions to be (intelligent) data wrappers, and Rules to handle all the, well, rules.

--- src/core/State.java
+++ src/core/State.java
@@ -4,9 +4,5 @@ package core;
  *
  * @author Ginger State represents a single state in a Game
  */
-public interface State<T extends Action>
-{
-    public boolean isTerminal();
-
-    public Score applyAction(final T action);
+public interface State {
 }
