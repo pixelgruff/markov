33ceac57d6261de1407bf7a0207ccf761443c98d
Stepchild <lee.matthan@gmail.com>
2015-07-11 05:00:36 -0400

    Migrated to using a PrintPolicy and playing a game with a LocalAutomator.

--- src/tetris/TetrisTest.java
+++ src/tetris/TetrisTest.java
@@ -1,11 +1,12 @@
 package tetris;
 
+import core.Automator;
 import core.Player;
+import core.Policy;
+import core.automators.LocalAutomator;
+import core.policies.PrintPolicy;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
 
 /**
  *
@@ -15,39 +16,20 @@ public class TetrisTest {
 
     public static void main(String[] args) {
 
-        /* Create a game of Tetris */
-        final TetrisRules tetrisRules = new TetrisRules();
-        final Set<Player> players = new HashSet<Player>();
-        final Player human = new Player("Human");
-        players.add(human);
-        TetrisState tetrisState = tetrisRules.generateInitialState(players);
-
-        /* Map available actions to String representations, and vice-versa */
+        /* Map available actions to String representations */
         final Map<String, TetrisAction> actionMap = new HashMap<>();
         actionMap.put("a", TetrisAction.LEFT);
         actionMap.put("d", TetrisAction.RIGHT);
         actionMap.put("s", TetrisAction.DROP);
         actionMap.put("w", TetrisAction.ROTATE);
         actionMap.put(" ", TetrisAction.WAIT);
-
-        /* Play the game with text output */
-        Scanner scan = new Scanner(System.in);
-        /* Begin game loop */
-        System.out.printf("Starting state: \n%s\n", tetrisState.toString());
-        System.out.printf("(%s) $ ", tetrisRules.getAvailableActions(human, tetrisState).toString());
-        String input = scan.nextLine();
-        while (!tetrisRules.isTerminal(tetrisState) && !input.equalsIgnoreCase("q")) {
-            if (!actionMap.containsKey(input)) {
-                System.err.println("Command not found!");
-            } else if (!tetrisRules.getAvailableActions(human, tetrisState).contains(actionMap.get(input))) {
-                System.err.println("Command not valid!");
-            } else {
-                tetrisState = tetrisRules.transition(tetrisState, actionMap.get(input));
-            }
-
-            System.out.printf("Current state: \n%s\n", tetrisState.toString());
-            System.out.printf("(%s) $ ", tetrisRules.getAvailableActions(human, tetrisState).toString());
-            input = scan.nextLine();
-        }
+        /* Create policy */
+        Policy stdOutPolicy = new PrintPolicy(actionMap);
+        /* Map a human player to this policy */
+        Map<Player, Policy> policyMap = new HashMap<>();
+        policyMap.put(new Player("Human"), stdOutPolicy);
+        /* Create the automator and run the game */
+        Automator automator = new LocalAutomator(new TetrisRules(), policyMap);
+        automator.playGameToCompletion();
     }
 }
